92236e36f29833b0c536ee41d67511ec431d9843
Leon <LeonShaw_zh@zju.edu.cn>
2025-03-12 18:06:31 +0800

    config update

---
+++ aoapc-book/tools.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include <cstdio>
+#include <cstring>
+#include <string>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+
+#include <algorithm>
+#include <cmath>
+
+#define REP(i, n) for (int i = 0; i < (int)(n); i++)
+#define FOR(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define FORD(i, a, b) for (int i = (int)(a); i >= (int)(b); i--)
+
+using namespace std;
+
